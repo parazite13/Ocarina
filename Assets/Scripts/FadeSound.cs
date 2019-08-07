727d1c6e112077aedea9d83f16c93965a33371a3
parazite13 <a.monnot13@gmail.com>
2019-08-07 17:23:50 +0200

    Sounds fixes so that there is no cracking at all

---
+++ Assets/Scripts/FadeSound.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ocarina
+{
+    public class FadeSound : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSource audioSource;
+
+        [SerializeField]
+        private float fadingTime;
+
+        public void Play()
+        {
+            StopAllCoroutines();
+            audioSource.volume = 1f;
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+
+        public void Stop()
+        {
+            StartCoroutine(FadeAudioSourceSoundCoroutine());
+        }
+
+        private IEnumerator FadeAudioSourceSoundCoroutine()
+        {
+            var startingTime = Time.realtimeSinceStartup;
+            while(Time.realtimeSinceStartup < startingTime + fadingTime)
+            {
+                var step = Time.deltaTime / fadingTime;
+                audioSource.volume -= step;
+                yield return null;
+            }
+            audioSource.loop = false;
+            audioSource.Stop();
+            audioSource.volume = 1f;
+        }
+    }
+}
